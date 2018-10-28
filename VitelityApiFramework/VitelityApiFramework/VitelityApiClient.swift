4cff8dd0e503671b9473d088b2b5425f714889a4
Jorge Enrique Dominguez <jorge@Jorges-MacBook-Pro.local>
2018-10-28 13:00:10 -0400

    New FW version

--- VitelityApiFramework/VitelityApiFramework/VitelityApiClient.swift
+++ VitelityApiFramework/VitelityApiFramework/VitelityApiClient.swift
@@ -1,6 +1,6 @@
 //
 //  VitelityApiClient.swift
-//  VitelityApiFramwork
+//  VitelityApiFramework
 //
 //  Created by Jorge Enrique Dominguez on 10/28/18.
 //  Copyright © 2018 sigmatric. All rights reserved.
@@ -8,11 +8,11 @@
 
 import Foundation
 
+
 public class VitelityApiClient {
     
     public static func getUUID() -> String {
-          return UUID().uuidString
-    }
-    
+        return UUID().uuidString
+    }    
     
 }
---
+++ VitelityApiFramework/VitelityApiFramework/VitelityApiFramework.h
@@ -0,0 +1,19 @@
+//
+//  VitelityApiFramework.h
+//  VitelityApiFramework
+//
+//  Created by Jorge Enrique Dominguez on 10/28/18.
+//  Copyright © 2018 sigmatric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VitelityApiFramework.
+FOUNDATION_EXPORT double VitelityApiFrameworkVersionNumber;
+
+//! Project version string for VitelityApiFramework.
+FOUNDATION_EXPORT const unsigned char VitelityApiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VitelityApiFramework/PublicHeader.h>
+
+
